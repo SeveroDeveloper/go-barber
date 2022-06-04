98d1ce68c0c2b17f0a3c2b86373cc3ae1594b7ee
Amanda Severo <amandasevero.work@gmail.com>
2022-06-04 19:43:45 -0300

    feat(users): using JWT to cryptography

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,43 @@
+import { compare } from 'bcryptjs';
+import { getRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response
+{
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, '60623cda1643e57276e8387f076db8cc', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
